9247aeff6ca8bf57ee2ff6e7b44f0a4578d376bb
Martin <57033944+saugstauberr@users.noreply.github.com>
2023-10-06 04:35:44 +0200

    Add classes and packages

---
+++ src/test/java/FilterIP.java
@@ -0,0 +1,5 @@
+public class FilterIP {
+    public static void main(String[] args) {
+        System.out.println("Main method working!");
+    }
+}
---
+++ src/test/java/FilterTime.java
@@ -0,0 +1,5 @@
+public class FilterTime {
+    public static void main(String[] args) {
+        System.out.println("Main method working!");
+    }
+}
